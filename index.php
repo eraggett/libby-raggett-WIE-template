851a9fc5077a61d10e2c12705c13522599f88bb4
eraggett2 <eraggett@greatergood.com>
2016-02-15 23:44:25 -0500

    add skeleton

--- index.php
+++ index.php
@@ -11,15 +11,23 @@
 </head>
 
  <body>
-	<header>
-            <h1>Blank WordPress Template</h1>
-            <p>This is my WordPress template.</p>
-        </header>
-        <section>
-            <p>Body content goes here.</p>
-        </section>
-        <footer>
-            <p>Footer content goes here.</p>
-        </footer>
+ <div class="container">
+            <header class="row">
+                <div class="twelve columns">
+                    <h1>Blank WordPress Template</h1>
+                    <p>This is my WordPress template.</p>
+                </div>
+            </header>
+            <section class="row">
+                <div class="twelve columns">
+                    <p>Body content goes here.</p>
+                </div>
+            </section>
+            <footer class="row">
+                <div class="twelve columns">
+                    <p>Footer content goes here.</p>
+                </div>
+            </footer>
+        </div> <!-- ends container -->
 </body>
 </html>
