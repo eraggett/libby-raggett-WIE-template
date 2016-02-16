b9eaf8cdf0b66f68f1f34f69c987e1875448c4c9
eraggett2 <eraggett@greatergood.com>
2016-02-15 23:59:59 -0500

    link sidebar.php on page.php

--- page.php
+++ page.php
@@ -1,20 +1,16 @@
 <?php get_header(); ?>
     <div class="row">
-
-<!-- MAKE THIS COLUMN NINE WIDTH -->
         <div class="nine columns">
             <?php if (have_posts()) :           
-                while (have_posts()) : the_post(); ?> 
+while (have_posts()) : the_post(); ?> 
                     <h2><?php the_title(); ?></h2>
                     <?php the_content();
                 endwhile;
             endif; ?>
         </div>
-<!-- ADD IN A THREE WIDTH COLUMN - COMPLETE THE TWELVE REQUIREMENT WIDTH -->
 <!-- BEGIN SIDEBAR -->
         <div class="three columns">
-            <h3>Sidebar Text</h3>
-            <p>This is the sidebar content to put in here.</p>
+            <?php get_sidebar(); ?>
         </div>
 <!-- END SIDEBAR -->
     </div>
