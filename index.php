6b76e6764632a3e4e17123762d1240cb6a29fdf6
eraggett2 <eraggett@greatergood.com>
2016-02-16 01:30:29 -0500

    edit loop

--- index.php
+++ index.php
@@ -9,7 +9,9 @@
             <?php 
             if ( have_posts() ) {
                 while ( have_posts() ) {
-                    the_post(); 
+                    the_post();?>
+                    <h3><?php the_title(); ?></h3>
+                    <?php the_excerpt();
                 } // end while
             } // end if
             ?>
