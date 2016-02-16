2e8403b61389ae5492393267a18715fb8e0c3cfc
eraggett2 <eraggett@greatergood.com>
2016-02-15 23:47:27 -0500

    populate header file

--- header.php
+++ header.php
@@ -2,9 +2,19 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<!-- Link to a CSS file --> 
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+<!-- Link to a JavaScript file --> 
+<script src="custom.js"></script>
 <title>Untitled Document</title>
 </head>
 
-<body>
-</body>
-</html>
+ <body>
+ <div class="container">
+            <header class="row">
+                <div class="twelve columns">
+                    <h1>Blank WordPress Template</h1>
+                    <p>This is my WordPress template.</p>
+                </div>
+            </header>
