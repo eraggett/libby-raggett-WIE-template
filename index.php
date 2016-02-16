f09d15e9ea612e98cbd8bfc7dd99dab5fffbd369
eraggett2 <eraggett@greatergood.com>
2016-02-15 23:53:44 -0500

    add loop in index

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
-
-           <?php get_header(); ?>
-
-            <section class="row">
-                <div class="twelve columns">
-                    <p>Body content goes here.</p>
-                </div>
-            </section>
-            <?php get_footer(); ?>
-
-            
+<?php get_header(); ?>
+    <section class="row">
+        <div class="twelve columns">
+<!-- BEGIN LOOP -->
+            <?php 
+            if ( have_posts() ) {
+                while ( have_posts() ) {
+                    the_post(); 
+                } // end while
+            } // end if
+            ?>
+<!-- END LOOP -->
+        </div>
+    </section>
+<?php get_footer(); ?>
