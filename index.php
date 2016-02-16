5c53647eec8258b59b36955e987ada6dc625399e
eraggett2 <eraggett@greatergood.com>
2016-02-15 23:49:40 -0500

    add header and footer functions

--- index.php
+++ index.php
@@ -1,33 +1,11 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<!-- Link to a CSS file --> 
-<link rel="stylesheet" type="text/css" href="style.css" />
 
-<!-- Link to a JavaScript file --> 
-<script src="custom.js"></script>
-<title>Untitled Document</title>
-</head>
+           <?php get_header(); ?>
 
- <body>
- <div class="container">
-            <header class="row">
-                <div class="twelve columns">
-                    <h1>Blank WordPress Template</h1>
-                    <p>This is my WordPress template.</p>
-                </div>
-            </header>
             <section class="row">
                 <div class="twelve columns">
                     <p>Body content goes here.</p>
                 </div>
             </section>
-            <footer class="row">
-                <div class="twelve columns">
-                    <p>Footer content goes here.</p>
-                </div>
-            </footer>
-        </div> <!-- ends container -->
-</body>
-</html>
+            <?php get_footer(); ?>
+
+            
