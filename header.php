9cd8fb35309a5992524717123d3c631f66b6d09f
eraggett2 <eraggett@greatergood.com>
2016-02-16 03:05:33 -0500

    fixed

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
             <header class="row">
                 <div class="twelve columns">
                 
-                     <h1><a  href="<?php the_permalink();?>bloginfo('name'); ?></a></h1>
+                     <h1><a  href="<?php the_permalink(); ?>"><?php bloginfo('name'); ?></a></h1>
                     <h2><?php bloginfo('description'); ?></h2>
                 </div>
             </header>
