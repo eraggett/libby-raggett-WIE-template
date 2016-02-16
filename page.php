ad7f6368ac8975e259c61c6aa171ecab5e696c48
eraggett2 <eraggett@greatergood.com>
2016-02-15 23:55:10 -0500

    add function on page.php

--- page.php
+++ page.php
@@ -1,10 +1,15 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+<?php get_header(); ?>
+    <div class="row">
+        <div class="twelve columns">
+<!-- BEGIN PAGE PHP -->
+            <?php if (have_posts()) : 
+                /* OUR DATA CONTEXT IS DEFINED  */      
+                while (have_posts()) : the_post(); ?> 
+                    <h2><?php the_title(); ?></h2>
+                    <?php the_content();
+                endwhile;
+            endif; ?>
+<!-- END PAGE PHP -->
+        </div>
+    </div>
+<?php get_footer(); ?>
