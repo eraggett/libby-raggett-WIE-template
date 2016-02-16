07d967389d7632eb6988968728cf6e11654a6163
eraggett2 <eraggett@greatergood.com>
2016-02-16 01:10:58 -0500

    setting up page

--- page.php
+++ page.php
@@ -1,17 +1,19 @@
-<?php get_header(); ?>
-    <div class="row">
-        <div class="nine columns">
-            <?php if (have_posts()) :           
-while (have_posts()) : the_post(); ?> 
-                    <h2><?php the_title(); ?></h2>
-                    <?php the_content();
-                endwhile;
-            endif; ?>
-        </div>
-<!-- BEGIN SIDEBAR -->
-        <div class="three columns">
-            <?php get_sidebar(); ?>
-        </div>
-<!-- END SIDEBAR -->
-    </div>
-<?php get_footer(); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+        <title>Blank Template</title>
+<?php wp_head(); ?>
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+        <?php wp_head(); ?>
+    </head>
+
+ <body>
+ <div class="container">
+            <header class="row">
+                <div class="twelve columns">
+                     <h1><?php bloginfo('name'); ?></h1>
+                    <h2><?php bloginfo('description'); ?></h2>
+                </div>
+            </header>
+            <!-- END OF HEADER - BEGIN SECTION CONTENT -->
