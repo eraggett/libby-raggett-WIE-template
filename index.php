96d8d3248d8210f7e1c989a9132fe9a0971bb294
eraggett2 <eraggett@greatergood.com>
2016-02-15 23:41:20 -0500

    link stylesheet

--- index.php
+++ index.php
@@ -2,9 +2,16 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<!-- Link to a CSS file --> 
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+<!-- Link to a JavaScript file --> 
+<script src="custom.js"></script>
 <title>Untitled Document</title>
 </head>
 
 <body>
+	<h1>Blank WordPress Template</h1>
+    <p>This is my WordPress template.</p>
 </body>
 </html>
